925f55ab8b63098d573b8d14fd8cfd3a1310621c
Bruno Silva Santana <ibrunoss@outlook.com>
2021-06-10 14:02:42 -0300

    Create SafeEnvironment.style component

---
+++ src/ui/components/feddback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -0,0 +1,8 @@
+import { Container } from "@material-ui/core";
+import { styled } from "@material-ui/core/styles";
+
+export const SafeEnvironmentContainerStyled: React.FC = styled(Container)`
+  text-align: right;
+  padding: 16px 0;
+  font-size: 12px;
+`;
