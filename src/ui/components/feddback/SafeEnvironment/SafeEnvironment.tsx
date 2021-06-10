60409e6639a1c4a2f79649e756e92cf243bf0f14
Bruno Silva Santana <ibrunoss@outlook.com>
2021-06-10 14:02:58 -0300

    Create SafeEnvironment component

---
+++ src/ui/components/feddback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { SafeEnvironmentContainerStyled } from "./SafeEnvironment.style";
+
+const SafeEnvironment: React.FC = () => (
+  <SafeEnvironmentContainerStyled>
+    Ambiente Seguro
+  </SafeEnvironmentContainerStyled>
+);
+
+export default SafeEnvironment;
