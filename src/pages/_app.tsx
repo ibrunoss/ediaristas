e858c114b982dd64e82bc4961bcdc70af2197610
Bruno Silva Santana <ibrunoss@outlook.com>
2021-06-16 17:28:37 -0300

    Apply theme to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from "@material-ui/core";
+
+import theme from "ui/themes";
 import "@styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
