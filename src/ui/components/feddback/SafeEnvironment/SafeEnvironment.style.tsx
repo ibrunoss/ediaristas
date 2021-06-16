c29f59392e73f55369811888fcffd688d113a5b8
Bruno Silva Santana <ibrunoss@outlook.com>
2021-06-16 18:09:51 -0300

    Change style of SafeEnvironmentContainerStyled

--- src/ui/components/feddback/SafeEnvironment/SafeEnvironment.style.tsx
+++ src/ui/components/feddback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -2,7 +2,9 @@ import { Container } from "@material-ui/core";
 import { styled } from "@material-ui/core/styles";
 
 export const SafeEnvironmentContainerStyled: React.FC = styled(Container)`
+  color: ${({ theme }) => theme.palette.text.secondary};
+  background-color: ${({ theme }) => theme.palette.background.default};
   text-align: right;
-  padding: 16px 0;
+  padding: ${({ theme }) => theme.spacing(2)} 0;
   font-size: 12px;
 `;
