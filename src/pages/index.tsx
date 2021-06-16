a19f19d2abdba7f82fe45756e4303f6bcb1191ea
Bruno Silva Santana <ibrunoss@outlook.com>
2021-06-16 17:31:55 -0300

    Apply PageTitle component to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Head from "next/head";
 
 import SafeEnvironment from "ui/components/feddback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
+      <PageTitle>
+        <>
+          <h1>Conheça os profissionais</h1>
+          <h2>
+            Preencha seu endereço e veja todos os profissionais da sua
+            localidade
+          </h2>
+        </>
+      </PageTitle>
     </div>
   );
 }
