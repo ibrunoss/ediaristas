8a92a1db1ae8fc936a689079f28748c5f5676e9d
Bruno Silva Santana <ibrunoss@outlook.com>
2021-06-18 21:53:46 -0300

    Apply UserInformation component to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import SafeEnvironment from "ui/components/feddback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
   return (
@@ -16,6 +17,12 @@ export default function Home() {
           </h2>
         </>
       </PageTitle>
+      <UserInformation
+        avatar="https://github.com/89bsilva.png"
+        description="Tatuí"
+        name="Bruno"
+        rating={5}
+      />
     </div>
   );
 }
