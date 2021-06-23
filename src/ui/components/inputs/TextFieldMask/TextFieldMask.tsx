991db4e2d24aa7327b3ba3d5db95f383bb98b88c
Bruno Silva Santana <ibrunoss@outlook.com>
2021-06-23 20:05:15 -0300

    TextFieldMask component props changed

--- src/ui/components/inputs/TextFieldMask/TextFieldMask.tsx
+++ src/ui/components/inputs/TextFieldMask/TextFieldMask.tsx
@@ -8,8 +8,15 @@ import TextField, {
 export interface TextFieldMaskProps extends TextFieldProps {
   mask: string;
 }
-const TextFieldMask: React.FC<TextFieldMaskProps> = ({ mask, ...props }) => (
-  <InputMask mask={mask}>{() => <TextField {...props} />}</InputMask>
+const TextFieldMask: React.FC<TextFieldMaskProps> = ({
+  mask,
+  value,
+  onChange,
+  ...props
+}) => (
+  <InputMask mask={mask} value={value} onChange={onChange}>
+    {() => <TextField {...props} />}
+  </InputMask>
 );
 
 export default TextFieldMask;
