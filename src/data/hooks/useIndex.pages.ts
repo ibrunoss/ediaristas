269af13f3bea5bd7facf353fb6b24c67f9167318
Bruno Silva Santana <ibrunoss@outlook.com>
2021-07-03 19:38:39 -0300

    searchProfessionalsByZipCode function created on useIndex.pages.ts

--- src/data/hooks/useIndex.pages.ts
+++ src/data/hooks/useIndex.pages.ts
@@ -1,7 +1,8 @@
 import { useState, useMemo } from "react";
 
-import { UserInterface } from "data/@types/UserInterface";
+import { UserInterface, UserInterfaceAPI } from "data/@types/UserInterface";
 import ValidationService from "data/services/ValidationService";
+import ApiService from "data/services/ApiService";
 
 export default function useIndex() {
   const [zipCode, setZipCode] = useState(""),
@@ -19,6 +20,44 @@ export default function useIndex() {
     [professionals, setProfessionals] = useState([] as UserInterface[]),
     [remainingProfessionals, setRemainingProfessionals] = useState(0);
 
+  async function searchProfessionalsByZipCode(zipCode: string) {
+    setSearchDone(false);
+    setSearching(true);
+    setError("");
+
+    try {
+      await ApiService.get<{
+        diaristas: UserInterfaceAPI[];
+        quantidade_diaristas: number;
+      }>(`/api/diaristas-cidade?cep=${zipCode.replace(/\D/g, "")}`).then(
+        ({ data }) => {
+          const { diaristas, quantidade_diaristas } = data;
+          const response: {
+            professionals: UserInterface[];
+            remainingProfessionals: number;
+          } = {
+            professionals: diaristas.map(
+              ({ cidade, nome_completo, foto_usuario, reputacao }) => ({
+                city: cidade,
+                photo: foto_usuario,
+                fullName: nome_completo,
+                rating: reputacao,
+              })
+            ),
+            remainingProfessionals: quantidade_diaristas,
+          };
+          setProfessionals(response.professionals);
+          setRemainingProfessionals(response.remainingProfessionals);
+        }
+      );
+    } catch (error) {
+      setError("CEP não encontrado");
+    }
+
+    setSearching(false);
+    setSearchDone(true);
+  }
+
   return {
     zipCode,
     setZipCode,
@@ -34,5 +73,6 @@ export default function useIndex() {
     setProfessionals,
     remainingProfessionals,
     setRemainingProfessionals,
+    searchProfessionalsByZipCode,
   };
 }
